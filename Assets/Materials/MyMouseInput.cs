6d860a300e0274cbf37113c52b848ae7203f3d66
Allie <56135023+ahollenbaugh@users.noreply.github.com>
2023-03-03 23:15:19 -0800

    Got shitty movement feature working sort of

--- Assets/Materials/MyMouseInput.cs
+++ Assets/Materials/MyMouseInput.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MyMouseInput : MonoBehaviour
 {
     public PrimitiveType objectType;
+    GameObject geomObj;
+    bool objPlaced = false;
 
     public void ChangePrimitiveTypeCube()
     {
@@ -24,74 +26,91 @@ public class MyMouseInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Set default object type:
         objectType = PrimitiveType.Cube;
 
-        // CreatePrimitive returns a GameObject
-        // PrimitiveType.Capsule is a PrimitiveType
+        // Create initial GameObject:
+        geomObj = GameObject.CreatePrimitive(objectType);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
-
-        if (Input.GetMouseButtonUp(0))  // check if left button is pressed
+        if (!objPlaced)
         {
-            // take mouse position, convert from screen space to world space, do a raycast, store output of raycast into 
-            // hitInfo object ...
-
-            #region Screen To World
+            // Try getting raycast data while you move the mouse around instead of only when it's clicked:
             RaycastHit hitInfo = new RaycastHit();
             bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo); // starting point of ray in world coordinates, direction of ray
-            
-            if (hit)
-            {
-                #region HIDE
-                var geomObj = GameObject.CreatePrimitive(objectType);
-                //cube.GetComponent<BoxCollider>().isTrigger = true;
-                //cube.GetComponent<Renderer>().material = blockMaterial;
-                #endregion
+            Debug.Log("Raycast coordinates: (" + hitInfo.point.x + ", " + hitInfo.point.y + ", " + hitInfo.point.z + ")");
+            geomObj.transform.position = new Vector3(hitInfo.point.x, 0, hitInfo.point.z);
 
-                #region HIDE
-                if (hitInfo.transform.name.Equals("Base") && !hitInfo.transform.tag.Equals("button"))
+            if (Input.GetMouseButtonUp(0))  // check if left button is pressed
+            {
+                // take mouse position, convert from screen space to world space, do a raycast, store output of raycast into 
+                // hitInfo object ...
+
+                #region Screen To World
+                //RaycastHit hitInfo = new RaycastHit();
+                //bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo); // starting point of ray in world coordinates, direction of ray
+
+                if (hit)
                 {
-                    geomObj.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + (0.5f), hitInfo.point.z);
+                    #region HIDE
+                    //var geomObj = GameObject.CreatePrimitive(objectType);
+                    //cube.GetComponent<BoxCollider>().isTrigger = true;
+                    //cube.GetComponent<Renderer>().material = blockMaterial;
+                    #endregion
+
+                    #region HIDE
+                    if (hitInfo.transform.name.Equals("Base") && !hitInfo.transform.tag.Equals("button"))
+                    {
+                        geomObj.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + (0.5f), hitInfo.point.z);
+                    }
+                    #region HIDE
+                    else
+                    {
+                        geomObj.transform.position = hitInfo.transform.position + (hitInfo.normal);
+                        geomObj.transform.rotation = Quaternion.LookRotation(hitInfo.normal, Vector3.up);
+                    }
+                    #endregion
+
+                    objPlaced = true;
+
+                    Debug.DrawRay(hitInfo.point, hitInfo.normal, Color.red, 2, false);
+                    Debug.Log(hitInfo.normal);
+                    #endregion
+
+
                 }
-                #region HIDE
                 else
                 {
-                    geomObj.transform.position = hitInfo.transform.position + (hitInfo.normal);
-                    geomObj.transform.rotation = Quaternion.LookRotation(hitInfo.normal, Vector3.up);
+                    Debug.Log("No hit");
                 }
                 #endregion
-
-                Debug.DrawRay(hitInfo.point, hitInfo.normal, Color.red, 2, false);
-                Debug.Log(hitInfo.normal);
-                #endregion
-
-
             }
-            else
-            {
-                Debug.Log("No hit");
-            }
-            #endregion
         }
-        else if (Input.GetMouseButtonUp(1))
+        else
         {
-            // If the user right-clicks on an object, it explodes.
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            if (hit)
-            {
-                if (!hitInfo.transform.name.Equals("Base"))
-                {
-                    var exp = hitInfo.transform.gameObject.AddComponent<TriangleExplosion>();
-                    StartCoroutine(exp.SplitMesh(true));
+            // Need to make a new object:
+            geomObj = GameObject.CreatePrimitive(objectType);
+            objPlaced = false;
 
-                }
-            }
         }
+        
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    // If the user right-clicks on an object, it explodes.
+        //    hitInfo = new RaycastHit();
+        //    hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+        //    if (hit)
+        //    {
+        //        if (!hitInfo.transform.name.Equals("Base"))
+        //        {
+        //            var exp = hitInfo.transform.gameObject.AddComponent<TriangleExplosion>();
+        //            StartCoroutine(exp.SplitMesh(true));
+
+        //        }
+        //    }
+        //}
     }
 }
